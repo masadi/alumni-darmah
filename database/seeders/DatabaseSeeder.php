af4b3b12fa97c0587bb91be3cb81e6f1c6cf61f4
masadi <masadi.com@gmail.com>
2023-06-13 08:25:08 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Role;
+use App\Models\User;
+use App\Models\Page;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,8 +15,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            PageSeeder::class,
+        //$this->call([
+        //    PageSeeder::class,
+        //]);
+        $role = Role::updateOrCreate([
+            'name' => 'admin',
+            'display_name' => 'Administrator',
+            'description' => 'Administrator Akses',
         ]);
+        $user = User::updateOrCreate(
+            [
+                'email' => 'admin@email.com'
+            ],
+            [
+                'name' => 'Administrator',
+                'password' => bcrypt('#B1mill4h@')
+            ],
+        );
+        if(!$user->hasRole('admin')){
+            $user->addRole($role);
+        }
     }
 }
